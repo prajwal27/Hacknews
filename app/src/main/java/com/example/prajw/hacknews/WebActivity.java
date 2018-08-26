05e13d8f6abc25c3e56d6e1fe678b9daa8f72d69
prajwal27 <prajwalsbelagavi.171co229@nitk.edu.in>
2018-08-27 04:49:10 +0530

    #close

---
+++ app/src/main/java/com/example/prajw/hacknews/WebActivity.java
@@ -0,0 +1,59 @@
+package com.example.prajw.hackernews.Common;
+
+import android.graphics.Bitmap;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
+import com.example.prajw.hacknews.R;
+
+public class WebActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private WebView webView;
+    private ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web);
+
+        toolbar = findViewById(R.id.toolbar);
+        webView = findViewById(R.id.web);
+        progressBar = findViewById(R.id.progress);
+
+        toolbar.setTitle("Top");
+        toolbar.setNavigationIcon(R.drawable.ic_backarrow);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
+        webView.setVisibility(View.INVISIBLE);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.setWebViewClient(new WebViewClient(){
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                progressBar.setVisibility(View.GONE);
+                webView.setVisibility(View.VISIBLE);
+            }
+        });
+
+        webView.loadUrl(getIntent().getStringExtra("url"));
+    }
+}
